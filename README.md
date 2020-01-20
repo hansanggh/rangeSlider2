b6dd8beab6f08cf86b3f556ca76bd7ef674b553d
hansanggh <59546218+hansanggh@users.noreply.github.com>
2020-01-20 17:06:05 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# rangeSlider2
+# GimhsSlider
+
+Input Range Slider module 커스텀하여 사용하기.
+
+![screenshot](./images/img.png)
+
+## 시작하기
+
+1. [./range.css](./range.css) 추가하기.
+2. [./range.js](./range.js) 추가하기.
+3. html 작성하기.
+
+### 태그 생성
+
+input 태그에 id를 넣어 생성한다.
+
+```
+<input id="myRange"/>
+```
+
+### 객체 생성
+
+Gaia3dSlider 객체 생성시 매개변수로 input태그의 id를 넣는다.
+
+```
+var gs = new Gaia3dSlider('myRange');
+```
+
+### 속성설정
+
+객체에 속성을 추가하여 원하는 range slider를 만들어 보자.
+
+1. Min/Max설정
+
+    슬라이더의 시작값과 마지막값을 지정해 범위를 설정할 수 있습니다.
+```
+gs.setMax(20);
+gs.setMin(5);
+```
+
+
+2. Step설정
+
+    range thumb가 이동하는 단위를 설정 할 수 있습니다.
+```
+gs.setStep(2);
+```
+
+
+3. Duration설정
+
+    슬라이더의 재생 속도를 설정할 수 있습니다.(ms단위임)
+```
+gs.setDuration(1000);
+```
---
+++ range.js
@@ -0,0 +1,191 @@
+var Gaia3dSlider = function (id) {
+
+    var defaultOptions = {
+        max: 5,
+        min: 0,
+        step: 1,
+        duration: 1000
+    };
+
+    this.id = id;
+    this.max = defaultOptions.max;
+    this.min = defaultOptions.min;
+    this.step = defaultOptions.step;
+    this.duration = defaultOptions.duration;
+    this.t = defaultOptions.min;
+
+    this.init(id);
+}
+
+
+Gaia3dSlider.prototype.init = function (id) {
+
+    if (document.getElementById(id) != null) {
+
+        var input = document.getElementById(id);
+
+        if (input.tagName == 'INPUT') {
+
+            var slider = document.getElementById(id);
+            var div = document.createElement('div');
+            var divChild1 = document.createElement('div');
+            var divChild2 = document.createElement('div');
+            div.setAttribute('class', 'rangeWrap');
+            divChild1.setAttribute('class', 'rangeWrapChild button');
+            divChild2.setAttribute('class', 'rangeWrapChild slide');
+            divChild2.append(slider);
+            div.append(divChild1);
+            div.append(divChild2);
+            document.body.appendChild(div);
+    
+            var max = this.max;
+            var min = this.min;
+            var step = this.step;
+            var duration = this.duration;
+
+            makeSliderBtn("startBtn", "start");
+            makeSliderBtn("stopBtn", "stop");
+            makeSliderBtn("beforeBtn", "before");
+            makeSliderBtn("afterBtn", "after");
+
+            setSlider(id, max, min, step, duration);
+
+            function makeSliderBtn(name, action) {
+                var name = document.createElement('input');
+                // var div = document.createElement('div');
+
+                // div.setAttribute('class', 'gaiaBtn');
+
+                name.id = action + id;
+                name.type = "button";
+                name.setAttribute('class', action);
+                name.value = action;
+                divChild1.append(name);
+            }
+
+            
+
+
+        } else {
+            throw '"' + id + '" 라는 id의 input이 없습니다.';
+        }
+
+    } else {
+        throw '"' + id + '" 라는 id의 input이 없습니다.';
+    }
+}
+
+Gaia3dSlider.prototype.setMax = function (max) {
+    this.max = max;
+
+    var max = max;
+    var min = this.min;
+    var step = this.step;
+    var duration = this.duration;
+
+    setSlider(this.id, max, min, step, duration);
+
+}
+
+Gaia3dSlider.prototype.setMin = function (min) {
+    this.min = min;
+
+    var max = this.max;
+    var min = min;
+    var step = this.step;
+    var duration = this.duration;
+
+    setSlider(this.id, max, min, step, duration);
+
+}
+
+Gaia3dSlider.prototype.setStep = function (step) {
+    this.step = step;
+
+    var max = this.max;
+    var min = this.min;
+    var step = step;
+    var duration = this.duration;
+
+    setSlider(this.id, max, min, step, duration);
+
+}
+
+Gaia3dSlider.prototype.setDuration = function (duration) {
+    this.duration = duration;
+
+    var max = this.max;
+    var min = this.min;
+    var step = this.step;
+    var duration = duration;
+
+    setSlider(this.id, max, min, step, duration);
+
+}
+
+var setSlider = function (id, max, min, step, duration) {
+
+    var slider = document.getElementById(id);
+
+    var t = min;
+    var startButton = document.getElementById("start" + id);
+    var stopButton = document.getElementById("stop" + id);
+    var beforeButton = document.getElementById("before" + id);
+    var afterButton = document.getElementById("after" + id);
+    var timer;
+
+    slider.value = t;
+    slider.type = 'range';
+    slider.max = max;
+    slider.min = min;
+    slider.step = step;
+    slider.className = 'slider';
+
+    startButton.onclick = start;
+    beforeButton.onclick = before;
+    afterButton.onclick = after;
+    slider.onclick = clickValue;
+
+    function clickValue() {
+        t = new Number(slider.value);
+    }
+
+    function start() {
+        startButton.onclick = null;
+        stopButton.onclick = stop;
+
+        timer = setInterval(function () {
+
+            if (t == max) {
+                clearInterval(timer);
+                t = min;
+                slider.value = t;
+                startButton.onclick = start;
+
+            } else {
+                t = (t + step);
+                slider.value = t;
+
+            }
+        }, duration);
+    }
+    function stop() {
+        clearInterval(timer);
+        startButton.onclick = start;
+    }
+    function before() {
+        if (t <= max & t >= min) {
+
+            t = t - step;
+        }
+        slider.value = t;
+    }
+    function after() {
+        if (t < max) {
+
+            t = t + step;
+        }
+        slider.value = t;
+
+    }
+}
