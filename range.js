688eb77ccecbe71032eccf38802c147a2d781b80
hansanggh <59546218+hansanggh@users.noreply.github.com>
2020-01-21 11:52:21 +0900

    Update range.js

--- range.js
+++ range.js
@@ -1,4 +1,4 @@
-var KotSlider = function (id) {
+var KotSlider = function (inputId) {
 
     var defaultOptions = {
         max: 5,
@@ -6,27 +6,27 @@ var KotSlider = function (id) {
         step: 1,
         duration: 1000
     };
-
-    this.id = id;
-    this.max = defaultOptions.max;
-    this.min = defaultOptions.min;
-    this.step = defaultOptions.step;
-    this.duration = defaultOptions.duration;
+    
+    this.id = inputId;
     this.t = defaultOptions.min;
 
-    this.init(id);
+    this.sliderObject = {
+        id : inputId,
+        max : defaultOptions.max,
+        min : defaultOptions.min,
+        step : defaultOptions.step,
+        duration : defaultOptions.duration
+    };
+
+    this.init();
 }
 
+KotSlider.prototype.init = function () {
 
-KotSlider.prototype.init = function (id) {
 
-    if (document.getElementById(id) != null) {
+        if (document.getElementById(this.id) && document.getElementById(this.id).tagName == 'INPUT') {
+            var slider = document.getElementById(this.id);
 
-        var input = document.getElementById(id);
-
-        if (input.tagName == 'INPUT') {
-
-            var slider = document.getElementById(id);
             var div = document.createElement('div');
             var divChild1 = document.createElement('div');
             var divChild2 = document.createElement('div');
@@ -37,23 +37,18 @@ KotSlider.prototype.init = function (id) {
             div.append(divChild1);
             div.append(divChild2);
             document.body.appendChild(div);
-    
-            var max = this.max;
-            var min = this.min;
-            var step = this.step;
-            var duration = this.duration;
 
-            makeSliderBtn("startBtn", "start");
-            makeSliderBtn("stopBtn", "stop");
-            makeSliderBtn("beforeBtn", "before");
-            makeSliderBtn("afterBtn", "after");
+            makeSliderBtn("startBtn", "start", this.sliderObject.id);
+            makeSliderBtn("stopBtn", "stop", this.sliderObject.id);
+            makeSliderBtn("beforeBtn", "before", this.sliderObject.id);
+            makeSliderBtn("afterBtn", "after", this.sliderObject.id);
 
-            setSlider(id, max, min, step, duration);
+            this.setSlider(this.sliderObject);
 
-            function makeSliderBtn(name, action) {
+            function makeSliderBtn(name, action, id) {
                 var name = document.createElement('input');
                 // var div = document.createElement('div');
-
+                console.log(id);
                 // div.setAttribute('class', 'gaiaBtn');
 
                 name.id = action + id;
@@ -62,83 +57,55 @@ KotSlider.prototype.init = function (id) {
                 name.value = action;
                 divChild1.append(name);
             }
-
-            
-
-
         } else {
-            throw '"' + id + '" 라는 id의 input이 없습니다.';
+            throw '"' + this.id + '" 라는 id의 input이 없습니다.';
         }
-
-    } else {
-        throw '"' + id + '" 라는 id의 input이 없습니다.';
-    }
 }
 
 KotSlider.prototype.setMax = function (max) {
-    this.max = max;
-
-    var max = max;
-    var min = this.min;
-    var step = this.step;
-    var duration = this.duration;
-
-    setSlider(this.id, max, min, step, duration);
+    
+    this.sliderObject.max = max;
+    this.setSlider(this.sliderObject);
 
 }
 
 KotSlider.prototype.setMin = function (min) {
-    this.min = min;
-
-    var max = this.max;
-    var min = min;
-    var step = this.step;
-    var duration = this.duration;
-
-    setSlider(this.id, max, min, step, duration);
+    
+    this.sliderObject.min = min;
+    this.setSlider(this.sliderObject);
 
 }
 
 KotSlider.prototype.setStep = function (step) {
-    this.step = step;
-
-    var max = this.max;
-    var min = this.min;
-    var step = step;
-    var duration = this.duration;
-
-    setSlider(this.id, max, min, step, duration);
+    
+    this.sliderObject.step = step;
+    this.setSlider(this.sliderObject);
 
 }
 
 KotSlider.prototype.setDuration = function (duration) {
-    this.duration = duration;
-
-    var max = this.max;
-    var min = this.min;
-    var step = this.step;
-    var duration = duration;
-
-    setSlider(this.id, max, min, step, duration);
+    
+    this.sliderObject.duration = duration;
+    this.setSlider(this.sliderObject);
 
 }
 
-var setSlider = function (id, max, min, step, duration) {
+KotSlider.prototype.setSlider = function (sliderObject) {
 
-    var slider = document.getElementById(id);
+    var slider = document.getElementById(sliderObject.id);
 
-    var t = min;
-    var startButton = document.getElementById("start" + id);
-    var stopButton = document.getElementById("stop" + id);
-    var beforeButton = document.getElementById("before" + id);
-    var afterButton = document.getElementById("after" + id);
+    var t = sliderObject.min;
+    var startButton = document.getElementById("start" + sliderObject.id);
+    var stopButton = document.getElementById("stop" + sliderObject.id);
+    var beforeButton = document.getElementById("before" + sliderObject.id);
+    var afterButton = document.getElementById("after" + sliderObject.id);
     var timer;
 
     slider.value = t;
     slider.type = 'range';
-    slider.max = max;
-    slider.min = min;
-    slider.step = step;
+    slider.max = sliderObject.max;
+    slider.min = sliderObject.min;
+    slider.step = sliderObject.step;
     slider.className = 'slider';
 
     startButton.onclick = start;
@@ -156,35 +123,35 @@ var setSlider = function (id, max, min, step, duration) {
 
         timer = setInterval(function () {
 
-            if (t + step >= max) {
+            if (t + sliderObject.step >= sliderObject.max) {
                 clearInterval(timer);
-                t = min;
+                t = sliderObject.min;
                 slider.value = t;
                 startButton.onclick = start;
 
             } else {
-                t = (t + step);
+                t = (t + sliderObject.step);
                 slider.value = t;
 
             }
-        }, duration);
+        }, sliderObject.duration);
     }
     function stop() {
         clearInterval(timer);
         startButton.onclick = start;
     }
     function before() {
-        if (t - step <= max & t - step >= min) {
+        if (t - sliderObject.step <= sliderObject.max && t - sliderObject.step >= sliderObject.min) {
 
-            t = t - step;
+            t = t - sliderObject.step;
         }
         console.log(t);
         slider.value = t;
     }
     function after() {
-        if (t + step <= max) {
+        if (t + sliderObject.step <= sliderObject.max) {
 
-            t = t + step;
+            t = t + sliderObject.step;
         }
         console.log(t);
         slider.value = t;
